d17fbbe2d18f1ea680d8c1f279e8847a9cb2e843
Chira2000 <95146759+Chira2000@users.noreply.github.com>
2024-01-09 19:33:51 +0530

    initial commit

---
+++ WebApplication3/Models/Department.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication3.Models
+{
+    public class Department
+    {
+        [Key]
+
+    public int Department_ID { get; set; }
+    public string Department_Code { get; set;}
+
+    public string Department_Name { get; set;}
+    }
+}
